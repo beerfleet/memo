5cc3c95515a0a9ee7f952e33b27bd7ac1f8be159
Beerfleet <devel.janvb@gmail.com>
2015-08-16 18:42:32 +0200

    Config FOSUser

---
+++ src/Jvb/Memories/MainBundle/Entity/Repository/MUserRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Jvb\Memories\MainBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Description of MUserRepository
+ *
+ * @author jan van biervliet
+ */
+class MUserRepository extends EntityRepository {
+  //put your code here
+}
